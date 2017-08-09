132f42a0f08536973a80282527144fde0943f513
Michael Mao <michael.mao@eventmobi.com>
2017-08-09 18:30:22 -0400

    Add firebase backend.

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,8 +1,25 @@
+import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import ListItem from './ListItem';
 import '../css/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize Firebase
+    var config = {
+
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().signInAnonymously().catch(function(error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      console.log('ERROR:', errorCode, errorMessage);
+    });
+  }
+
   render() {
     var a = ['a','b'];
     return (
