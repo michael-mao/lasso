559d689ef19d2dc665be6bfc4b76fca4b7f98f2e
Michael Mao <michael.mao@eventmobi.com>
2017-08-29 20:26:57 -0400

    Added login form.

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,22 +1,27 @@
 import * as firebase from 'firebase';
 import React, { Component } from 'react';
+
 import Button from './Button';
 import ListItem from './ListItem';
 import '../css/App.css';
-import * as moment from 'moment';
+
+const STAGE = Object.freeze({LOGIN: 1, LIST: 2, CREATE: 3});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       outings: [],
-      showCreateForm: false,
-      name: '',
-      time: ''
+      stage: STAGE.LOGIN,
+      outingName: '',
+      outingTime: '',
+      userEmail: '',
+      userPassword: ''
     };
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.toggleCreateForm = this.toggleCreateForm.bind(this);
 
     // Initialize Firebase
     let config = {
@@ -32,7 +37,7 @@ class App extends Component {
     firebase.auth().signInAnonymously().catch(error => {
       let errorCode = error.code;
       let errorMessage = error.message;
-      console.log('ERROR:', errorCode, errorMessage); 
+      console.log('ERROR:', errorCode, errorMessage);
     });
   }
 
@@ -59,9 +64,9 @@ class App extends Component {
     // detach listeners
     firebase.database().off();
   }
-  
+
   toggleCreateForm() {
-    this.setState({ showCreateForm: !this.state.showCreateForm });
+    this.setState({ stage: STAGE.CREATE });
   };
 
   writeUserData(name, time) {
@@ -73,19 +78,32 @@ class App extends Component {
     });
   };
 
-  handleNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleTimeChange(event) {
-    this.setState({ time: event.target.value });
-  }
-
   handleSubmit(event) {
-    this.writeUserData(this.state.name, this.state.time);
-    this.toggleCreateForm();
+    this.writeUserData(this.state.outingName, this.state.outingTime);
+    this.setState({
+      stage: STAGE.LIST
+    });
     event.preventDefault();
   };
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleLogin(event) {
+    event.preventDefault();
+    console.log(this.state.userEmail, this.state.userPassword);
+
+    this.setState({
+      stage: STAGE.LIST
+    });
+  }
 
   render() {
     const outingItems = this.state.outings.map(outing => {
@@ -96,19 +114,35 @@ class App extends Component {
                        right="true"/>;
     });
 
-    let toggleCreateForm = () =>
-      this.setState({ showCreateForm: !this.state.showCreateForm });
-
     return (
       <div className="App">
         <h3 className="c-heading u-centered">Wed Oct 24</h3>
-        
 
-        { this.state.showCreateForm
+        { this.state.stage === STAGE.LOGIN
+            ? <form onSubmit={this.handleLogin}>
+                <div className="o-form-element">
+                  <div className="c-input-group c-input-group--stacked">
+                      <div className="o-field">
+                          <input className="c-field" name="userEmail" placeholder="Email"
+                            value={this.state.userEmail}
+                            onChange={this.handleInputChange}/>
+                      </div>
+                      <div className="o-field">
+                          <input className="c-field" name="userPassword" placeholder="Password" type="password"
+                            value={this.state.userPassword}
+                            onChange={this.handleInputChange}/>
+                      </div>
+                  </div>
+                </div>
+                <div className="o-form-element">
+                  <button className="c-button c-button--brand c-button--block" type="submit">Login</button>
+                </div>
+              </form>
+            : this.state.stage === STAGE.CREATE
             ? <div className="section">
                 <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="name"  value={this.state.name} onChange={this.handleNameChange} className="text-input" placeholder="Outing Name" />
-                  <input type="time"  value={this.state.time} onChange={this.handleTimeChange} className="text-input" />
+                  <input type="text" name="outingName" value={this.state.outingName} onChange={this.handleInputChange} className="text-input" placeholder="Outing Name" />
+                  <input type="time" name="outingTime" value={this.state.outingTime} onChange={this.handleInputChange} className="text-input" />
                   <div className="padded-section">
                     <Button full="true" className="text-input" success="true" title="OK" type="submit" />
                   </div>
@@ -117,7 +151,7 @@ class App extends Component {
             : <div>
                 {outingItems}
                 <div className="section">
-                  <Button full="true" success="true" title="CREATE" onClick={toggleCreateForm} />
+                  <Button full="true" success="true" title="CREATE" onClick={this.toggleCreateForm} />
                 </div>
               </div>
         }
